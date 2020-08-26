b9c7066b56900e53319a8e9e9a593ad1e7afb7b1
Lefcott <leandro.cotti@widergy.com>
2020-08-26 03:53:43 -0300

    redis sockets per user

---
+++ server/routes/socket.io/constants/connection.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+  USER_SOCKETS_KEY: 'user_sockets',
+  receive: { I_AM_X: 'I_AM_X' },
+  emit: { WHO_ARE_YOU: 'WHO_ARE_YOU' },
+  broadcast: {}
+});
--- server/routes/socket.io/index.js
+++ server/routes/socket.io/index.js
@@ -1,10 +1,24 @@
 const { socketIo } = require('../../utils/middlewares');
+const env = require('../../../env.json');
+const redis = require('../../utils/redis');
 
 const account = require('./account');
+const { USER_SOCKETS_KEY, receive, emit } = require('./constants/connection');
 
 socketIo.sockets.on('connect', socket => {
-  console.log('Client connected');
+  let userId;
+
+  socket.emit(emit.WHO_ARE_YOU);
+  socket.on(receive.I_AM_X, async _userId => {
+    const added = await redis.Add(USER_SOCKETS_KEY, { userId: _userId, socketId: socket.id });
+    if (added) userId = _userId;
+  });
+  socket.on('disconnect', () => {
+    if (userId) redis.Delete(USER_SOCKETS_KEY, { socketId: socket.id });
+  });
   account.define(socket);
 });
+
+redis.Delete(USER_SOCKETS_KEY, {});
 
 module.exports = { socketIo };
