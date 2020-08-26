a0b44c2429a7a08534a06b2a7e2ebe3d9b661836
Lefcott <leandro.cotti@widergy.com>
2020-08-26 04:25:16 -0300

    fix

--- scripts/start.js
+++ scripts/start.js
@@ -2,6 +2,6 @@ const { updateEnv } = require('./build/update_env');
 const { run } = require('./utils');
 
 (async () => {
-  await updateEnv();
+  await updateEnv(false);
   run('node --max-old-space-size=4096 app');
 })();
