28f2fe8f881288d6ace9a852678999b9eb3cb55e
Lefcott <leandro.cotti@widergy.com>
2020-08-30 18:11:08 -0300

    fix price

--- server/utils/account.js
+++ server/utils/account.js
@@ -1,4 +1,3 @@
-// TODO remove / 4
-const getAccountPrice = account => +(9.5 + (account.Level - 30) / 5).toFixed(2) / 4;
+const getAccountPrice = account => +(9.5 + (account.Level - 30) / 5).toFixed(2);
 
 module.exports = { getAccountPrice };
