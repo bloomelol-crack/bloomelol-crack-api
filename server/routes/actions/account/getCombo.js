16492bb4168173a60a9155f3252b8575af381d98
Lefcott <leandro.cotti@widergy.com>
2020-09-03 00:17:08 -0300

    ord by lvl

--- server/routes/actions/account/getCombo.js
+++ server/routes/actions/account/getCombo.js
@@ -2,24 +2,40 @@ const { account } = require('../../../database/models');
 
 module.exports = async (req, res) => {
   const { region, count, min_level } = req.body;
-  const Accounts = await account.aggregate([
+  let notFoundOnRegion = false;
+  let Accounts = await account.get(
     {
-      $match: {
+      NotInRegions: { $ne: region },
+      Level: { $gte: min_level },
+      Refunds: { $exists: false },
+      ...(region === 'any' ? {} : { FromUrl: new RegExp(`${region}.op.gg/`) })
+    },
+    { limit: count, sort: { Level: -1 }, projection: { _id: 0, UserName: 1, Password: 1, NewPassword: 1 } }
+  );
+  if (!Accounts) return res.status(500).json({ error: 'Problem finding accounts' });
+  if (!Accounts.length) {
+    notFoundOnRegion = true;
+    Accounts = await account.get(
+      {
         NotInRegions: { $ne: region },
         Level: { $gte: min_level },
-        Refunds: { $exists: false },
-        ...(region === 'any' ? {} : { FromUrl: new RegExp(`${region}.op.gg/`) })
-      }
-    },
-    { $sample: { size: count } },
-    { $project: { _id: 0, UserName: 1, Password: 1, NewPassword: 1 } }
-  ]);
-  if (!Accounts) return res.status(500).json({ error: 'Problem finding accounts' });
+        Refunds: { $exists: false }
+      },
+      { limit: count, sort: { $Level: -1 }, projection: { _id: 0, UserName: 1, Password: 1, NewPassword: 1 } }
+    );
+    if (!Accounts) return res.status(500).json({ error: 'Problem finding accounts' });
+  }
   if (!Accounts.length)
     return res
       .status(404)
       .send(`No encontramos cuentas con nivel mayor o igual a ${min_level} en "${region}".`);
   res
     .status(200)
-    .send(`${Accounts.map(acc => `${acc.UserName}:${acc.NewPassword || acc.Password}`).join('\n')}\n`);
+    .send(
+      `${
+        notFoundOnRegion
+          ? `INFO: ESTAS CUENTAS SON DE TODAS LAS REGIONES (no encontramos en "${region}.op.gg")\n\n`
+          : ''
+      }${Accounts.map(acc => `${acc.UserName}:${acc.NewPassword || acc.Password}`).join('\n')}\n`
+    );
 };
