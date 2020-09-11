4ad432d9bc72624b328efa2ca5d96b81e005799f
Lefcott <leandro.cotti@widergy.com>
2020-09-11 02:59:09 -0300

    login tracking

--- server/routes/actions/user/login.js
+++ server/routes/actions/user/login.js
@@ -1,4 +1,4 @@
-const { user } = require('../../../database/models');
+const { user, login } = require('../../../database/models');
 const { compare } = require('../../../utils/passwords');
 
 module.exports = async (req, res) => {
@@ -11,4 +11,5 @@ module.exports = async (req, res) => {
   req.session.user = User;
   req.session.save();
   res.status(200).json({ redirectTo: '/dashboard' });
+  login.save({ UserID: User._id, UserName: User.Name, UserSurname: User.Surname, IP: req.ip });
 };
