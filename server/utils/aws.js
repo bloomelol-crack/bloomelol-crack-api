1a41e40fb93f543f0c34298e18ca8fdbb9ed3ec1
Lefcott <leandro.cotti@widergy.com>
2020-09-10 23:20:55 -0300

    add vars

---
+++ server/utils/aws.js
@@ -0,0 +1,20 @@
+const Aws = require('aws-sdk');
+
+const env = require('./env');
+
+const { SES_REGION, SES_ACCESS_KEY, SES_SECRET_KEY } = env;
+const { SNS_REGION, SNS_ACCESS_KEY, SNS_SECRET_KEY } = env;
+
+const ses = new Aws.SES({
+  accessKeyId: SES_ACCESS_KEY,
+  secretAccessKey: SES_SECRET_KEY,
+  region: SES_REGION
+});
+
+const sns = new Aws.SNS({
+  accessKeyId: SNS_ACCESS_KEY,
+  secretAccessKey: SNS_SECRET_KEY,
+  region: SNS_REGION
+});
+
+module.exports = { ses, sns };
