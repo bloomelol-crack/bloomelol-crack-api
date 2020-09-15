2ef7b277a797a0b80fe3089bc73a8f04e944b7d8
Lefcott <leandro.cotti@widergy.com>
2020-09-14 23:40:58 -0300

    add clone

---
+++ scripts/build/clone_reaper.js
@@ -0,0 +1,5 @@
+const { run } = require('../utils');
+
+exports.cloneReaper = async () => {
+  await run('git clone https://bloomelol-crack:%40jv[gkVs92@github.com/Lefcott/AccountAutoreaper');
+};
--- scripts/build/index.js
+++ scripts/build/index.js
@@ -1,10 +1,13 @@
 require('colors');
 
 const { updateEnv } = require('./update_env');
+const { cloneReaper } = require('./clone_reaper');
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'localhost') {
   console.error('Cannot build on local environment, just run `npm run dev`\n'.red);
   process.exit(1);
 }
 
-updateEnv(true);
+updateEnv(true).then(() => {
+  cloneReaper();
+});
