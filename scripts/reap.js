e0912558c31b6c23cb5f26564443726dbf080cdc
Lefcott <leandro.cotti@widergy.com>
2020-09-15 12:17:44 -0300

    pull

--- scripts/reap.js
+++ scripts/reap.js
@@ -5,6 +5,7 @@ const { run } = require('./utils');
 let command = 'cd AccountAutoreaper';
 command += '; git config user.email "bloomelolcrack.addons@gmail.com"';
 command += '; git config user.name "bloomelol-crack"';
+command += '; git pull origin master';
 command += `; git commit -m "Autoreap ${moment().format('DD/MM/YYYY hh:mm A')}" --allow-empty`;
 command += '; git push origin master';
 
