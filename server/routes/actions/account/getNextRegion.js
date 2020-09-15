d470d80b7df5ca502cfd7d5247354b220ae02e39
lefcott19 <leandro.cotti@widergy.com>
2020-09-14 21:49:20 -0300

    add param

--- server/routes/actions/account/getNextRegion.js
+++ server/routes/actions/account/getNextRegion.js
@@ -2,6 +2,7 @@ const redis = require('../../../utils/redis');
 
 const { REGION_MAPPING } = require('./constants');
 
+const programRegions = Object.keys(REGION_MAPPING);
 const regions = Object.values(REGION_MAPPING);
 
 /**
@@ -10,7 +11,9 @@ const regions = Object.values(REGION_MAPPING);
 module.exports = async (req, res) => {
   let oldRegion = await redis.Get('current_region');
   oldRegion = oldRegion || regions[0];
-  const region = regions[(regions.indexOf(oldRegion) + 1) % regions.length];
-  res.status(200).json({ region });
+  const index = (regions.indexOf(oldRegion) + 1) % regions.length
+  const region = regions[index];
+  const programRegion = programRegions[index];
+  res.status(200).json({ region, programRegion });
   redis.Set('current_region', region);
 };
