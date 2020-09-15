a5d1feb8d2eeed3ea89fdf621c957914b618d52c
Lefcott <leandro.cotti@widergy.com>
2020-09-15 00:54:53 -0300

    fixxxx

--- server/tasks/reap/index.js
+++ server/tasks/reap/index.js
@@ -1,13 +1,13 @@
+const env = require('../../../env.json');
 const { run } = require('../../../scripts/utils');
 
 const { getRandomTime } = require('./utils');
 
 const fromTime = 1000 * 60 * 25; // 25 mins
 const toTime = 1000 * 60 * 35; // 35 mins
-
 const execute = () => {
   run('npm run reap');
   setTimeout(execute, getRandomTime(fromTime, toTime));
 };
 
-setTimeout(execute, getRandomTime(fromTime, toTime));
+if (env.NODE_ENV !== 'localhost') setTimeout(execute, getRandomTime(fromTime, toTime));
