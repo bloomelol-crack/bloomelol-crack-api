b750e1a7397a2493a2cfe1ec693b7bdb81708f7a
Lefcott <leandro.cotti@widergy.com>
2020-09-17 09:21:31 -0300

    fix babel

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,9 @@
-import mongoose, { connect } from 'mongoose';
+import mongoose from 'mongoose';
 
 import { MONGODB_URL } from '../../env.json';
 
 mongoose.set('useCreateIndex', true);
-connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, async error => {
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, async error => {
   if (error) return console.error(error);
   console.log('Connected to MongoDB!');
 });
