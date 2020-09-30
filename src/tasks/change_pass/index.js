6a4d8416df5fc500dc192fef1de80bbf5a6497a7
Lefcott <leandro.cotti@widergy.com>
2020-09-30 12:56:23 -0300

    fix

--- src/tasks/change_pass/index.js
+++ src/tasks/change_pass/index.js
@@ -34,7 +34,7 @@ const execute = async () => {
           .then(json => {
             if (json && json.error === 'auth_failure' && username) {
               account.delete({ UserName: username });
-              redis.Delete('passwordChangeRetries', { threadID: retry.threadID });
+              redis.Delete('passwordChangeRetries', { threadID: process.env.threadID });
               return rollbar.error(`Deleting account with bad credentials: ${username}:${password}`);
             }
             if (json && json['re-auth'] && json['re-auth'].region && username) {
