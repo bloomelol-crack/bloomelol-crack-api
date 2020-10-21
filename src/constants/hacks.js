3290615db01e1707883b7c05284d9664e6c14243
lefcott19 <leandro.cotti@widergy.com>
2020-10-21 04:55:30 -0300

    add_fnaf2

--- src/constants/hacks.js
+++ src/constants/hacks.js
@@ -8,5 +8,15 @@ export const HACKS = {
       { price: 4, months: 6 },
       { price: 7, months: 12 }
     ]
+  },
+  FNAF2: {
+    code: 'FNAF2',
+    name: "Five Nights At Freddy's 2",
+    licences: [
+      { price: 1.5, months: 1 },
+      { price: 2.5, months: 2 },
+      { price: 4, months: 6 },
+      { price: 7, months: 12 }
+    ]
   }
 };
